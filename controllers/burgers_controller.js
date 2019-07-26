5d79551f7e674a954c08c420631e1c66abc63314
Ellen N Evans <ellennewellevans@MacPro-EEvans.local>
2019-07-26 10:26:18 -0400

    continued updating app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,9 +16,9 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/cats", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   cat.create([
-    "name", "sleepy"
+    "burger_name", "devour"
   ], [
     req.body.name, req.body.sleepy
   ], function(result) {
@@ -27,7 +27,7 @@ router.post("/api/cats", function(req, res) {
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
@@ -44,7 +44,7 @@ router.put("/api/cats/:id", function(req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function(req, res) {
+router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   cat.delete(condition, function(result) {
