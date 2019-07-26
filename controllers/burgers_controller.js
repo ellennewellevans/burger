aa649717e725c748d0bff88ac543a26d46b96995
Ellen N Evans <ellennewellevans@MacPro-EEvans.local>
2019-07-26 10:48:57 -0400

    updated controllers and any other reference to cat

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,9 +7,9 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.create([
+  burger.insertOne([
     "burger_name", "devour"
   ], [
     req.body.name, req.body.sleepy
@@ -32,23 +32,10 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
+  burger.updateOne({
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,9 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var cat = {
-  SelectAll: function(cb) {
-    orm.SelectAll("burgers", function(res) {
+var burger = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
@@ -14,12 +14,7 @@ var cat = {
     });
   },
   updateOne: function(objColVals, condition, cb) {
-    orm.insertOne("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
